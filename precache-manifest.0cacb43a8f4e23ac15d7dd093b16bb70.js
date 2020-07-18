a0204d214e0950499fa54cbf4d6d930ce18d147c
soumith <soumithganji@gmail.com>
2020-07-18 14:50:37 +0530

    Updates

---
+++ precache-manifest.0cacb43a8f4e23ac15d7dd093b16bb70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb35cc4fa41cdc97f9fc7108a5fee998",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "49391e55e12a352bc16e",
+    "url": "/confusion/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f3e8ee8fc2c3455e61b0",
+    "url": "/confusion/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "49391e55e12a352bc16e",
+    "url": "/confusion/static/js/2.dd770e65.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/confusion/static/js/2.dd770e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e8ee8fc2c3455e61b0",
+    "url": "/confusion/static/js/main.93ed4089.chunk.js"
+  },
+  {
+    "revision": "47b295cfc61f796a9c36",
+    "url": "/confusion/static/js/runtime-main.19a3f2b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/confusion/static/media/logo.5d5d9eef.svg"
+  }
+]);
